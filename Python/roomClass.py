0c85a74443d4e7abec8fce1a18aaf817eafaf257
Brian Burrous <brianon007@gmail.com>
2021-07-31 11:50:27 -0700

    Updated notebook test

--- Python/roomClass.py
+++ Python/roomClass.py
@@ -3,13 +3,14 @@ from dataclasses import dataclass
 @dataclass()
 class room:
     name: str
-    length: float
-    width: float
-    cleanCode: str
-    furnitureCode: str
-    protectionCode: str
+    length: float = 0
+    width: float = 0
+    cleanCode: str = ""
+    furnitureCode: str = ""
+    protectionCode: str = ""
 
     def area(self) -> float:
         return self.length * self.width
 
-    
+    def formatList(self) ->list():
+        return [self.name, self.length, self.width, self.cleanCode, self.furnitureCode, self.protectionCode]
--- Python/roomScraper.py
+++ Python/roomScraper.py
@@ -1,24 +1,42 @@
 # data/94920teal0025.xls
 import xlrd
-import os
 from roomClass import room
 
-filename = 'data/94920teal0025.xls' 
-
-
-def printRooms(sheet):
+def extractRoomsData(sheet):
+    # Check to ensure sheet is properly structured
     if sheet.cell(17, 3).value == 'CARPETS AND RUGS':
-        i = 18
+        roomsList = list() # Create new list to store all rooms
+
+        i = 18 # Row index of first room
         while(sheet.cell(i, 3).value != 'UPHOLSTERY' and i < sheet.nrows):
+
+            # If cell contains an alphanumeric string, it is a room
+            # This data from this room is extracted
             if sheet.cell(i, 3).ctype != 0 and any(c.isalnum() for c in sheet.cell(i,3).value):
                 print("Room #{} with name {}".format((i-17), sheet.cell(i,3).value))
+
+                # Extract Revevant room properties and store in an instance of 
+                # class 'room' 
+                roomInstance = room(sheet.cell(i, 3).value)
+                roomInstance.length = sheet.cell(i, 4).value
+                roomInstance.width = sheet.cell(i, 5).value
+                roomInstance.cleanCode = sheet.cell(i, 7).value
+                roomInstance.furnitureCode = sheet.cell(i, 8).value
+                roomInstance.protectionCode = sheet.cell(i, 9).value
+
+                # Add roomInstance object of type 'room'
+                roomsList.append(roomInstance)
+
             i = i + 1
+
     else:
         print("error")
         print(sheet.cell(17,3))
 
-def callPrintRooms(sheetName):
-    book = xlrd.open_workbook(sheetName)
-    sheet = book.sheet_by_index(1)
-    printRooms(sheet)
+    return roomsList
+
+
+
+
+
 
