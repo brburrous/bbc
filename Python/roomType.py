685ccd7221c5391334762a8f366465355a4e4304
Brian Burrous <brianon007@gmail.com>
2021-08-09 10:33:35 -0700

    Function to detect stairs

--- Python/roomType.py
+++ Python/roomType.py
@@ -1,3 +1,27 @@
-def IsANote(sheet.cell(row, 7)):
-            return True
-    return False
+
+def IsANote(sheet, row):
+    lawSlice = sheet.row_slice(row, 4, 7) 
+    costSlice = sheet.row_slice(row, 10, 14)
+    slice = lawSlice + costSlice
+    tot = 0
+    for cell in slice:
+        if cell.value != 0:
+            tot += cell.ctype        
+    return tot == 0
+
+def isStairs(sheet, row):
+        lwSlice = sheet.row_slice(row, 4, 6)
+        tot = 0
+        for cell in slice:
+            if cell.value != 0:
+                tot += cell.ctype
+        if tot != 0:
+            return False    
+        return
+
+import re
+
+def inconsistentStairs():
+        p = re.compile('.*[sS][tT]')
+        cCodecell = sheet.cell(row, 7)
+        if p.match(cCodeCell):
